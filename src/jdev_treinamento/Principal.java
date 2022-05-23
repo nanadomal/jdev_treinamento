46e7e09b9e934a07e95b78b0167da3d7ece09343
nanadomal <python4052@gmail.com>
2022-05-23 00:20:11 -0300

    Isso é um teste de commit vamos verificar

--- src/jdev_treinamento/Principal.java
+++ src/jdev_treinamento/Principal.java
@@ -3,5 +3,6 @@ package jdev_treinamento;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("Isso é apenas um teste basico");
 	}
 }
