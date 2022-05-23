b04b20f2ae6a52a6845e8394154964a4f67aa0a1
nanadomal <python4052@gmail.com>
2022-05-23 00:27:43 -0300

    Isso é um commit de teste modificações e inclusões

--- src/jdev_treinamento/Principal.java
+++ src/jdev_treinamento/Principal.java
@@ -3,6 +3,7 @@ package jdev_treinamento;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Isso é apenas um teste basico");
+		System.out.println("modificamos o segundo teste, haha");
+		System.out.println("terceiro teste");
 	}
 }
