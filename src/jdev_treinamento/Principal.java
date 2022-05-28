28c267ef059fc2104e5351ecdfd84ffcd092a3ff
nanadomal <python4052@gmail.com>
2022-05-28 17:28:27 -0300

    modificando codigo

--- src/jdev_treinamento/Principal.java
+++ src/jdev_treinamento/Principal.java
@@ -3,7 +3,17 @@ package jdev_treinamento;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("modificamos o segundo teste, haha");
-		System.out.println("terceiro teste");
+		System.out.println("Isso é apenas um teste basico");
+		
+		//declarando um array 
+		int[] c = new int[12];
+		// declarando variavel de referentia 
+		int[] d;
+		d = new int[10];
+		
+		//podemos criar varios arrays em um declaração apenas 
+		String[] nomes = new String[50];
+		String sexo[] = new String[10];
+		
 	}
 }
